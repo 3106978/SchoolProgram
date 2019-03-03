0011f3a7531387d2cb3637422a5836b54f8bbbb4
Tanya kachanov <chernika.lisa@live.com>
2019-03-03 20:03:48 +0200

    /

--- SchoolProgram/Program.cs
+++ SchoolProgram/Program.cs
@@ -9,7 +9,36 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace SchoolProgram
-{
+{/// <summary>
+ /// This program is WEB API Application developed on the base of ASP.NET.CORE EntityFramwork 4.0 platform , with a database SQL SERVER EXPRESS, designed for primary school teachers and parents of pupils. To track attendance, teacher’s work schedule, and student’s class schedule, get contact details of pupil’s parents, and send messages to parents within the program.
+
+//    All users is already entered into the database by the administrator.The first entry into the program is performed with the initial login and password received from the administrator, then the user must sign up and enter a new password.
+
+//  The teacher can:
+//1. Check his work schedule in the selected time period;
+//2. Check the schedule of lessons of his class in the selected period of time;
+//3. Getting a list of pupils from his class with contact details of parents;
+//4. Sending a message to the parent of the selected pupil within the application;
+//5. Make pupil's attendance with the ability to comment on each student's work and view it.
+//6. Writing a general commentary to the lesson(for example, homework);
+
+//    Parent can:
+
+//1. View his child’s lesson schedule for a selected period of time and view a general comment on the lesson(for example, homework);
+//2. Receive and view messages from the teacher inside the application;
+//3. View attendance of his child and comments on his work in the classroom;
+
+//    Plans for further development:
+//- allowing the administrator to make changes to the database(student data, teacher data, lesson schedule, teacher work schedule)
+
+//To test the program, you can use the following data:
+
+//New User(Pearent): Login: Bobbi password:123asd
+//New User(Teacher) : Login: Mikky password:123asd
+
+//Teacher with data(attendance, schedule): password: 123asd, login:Rami
+//Pearent with techer's messages login:bubu password:123dfgg
+/// </summary>
     public class Program
     {
         public static void Main(string[] args)
